5dcc547250bc565511df75d4474e8af3f5e5314d
M.Tamura <tamura2004@gmail.com>
2017-09-25 18:22:22 +0900

    Add some command example

--- azure.rb
+++ azure.rb
@@ -16,7 +16,7 @@ class Azure
   def exec
     option_string = @option.map do |key, value|
       dash = key.size == 1 ? "-" : "--"
-      key = key.to_s.sub(/_/,"-")
+      key = key.to_s.gsub(/_/,"-")
       value = value.inspect if key.to_s == "query"
       "#{dash}#{key} #{value}"
     end
@@ -55,10 +55,11 @@ end
 #   )
 # end
 
+# Azure.run("az vm create", h: true)
 # Azure.run("az network vnet list", o: "table")
 # Azure.run(
 #   "az network vnet list",
-#   query: "[].{Location:location,Name:name,AddressPrefix:addressSpace.addressPrefixes[0],SubnetName1:subnets[0].name,SubnetAddress1:subnets[0].addressPrefix,SubnetName2:subnets[1].name,SubnetAddress2:subnets[1].addressPrefix}",
+#   query: "[].{Location:location,Name:name,ResourceGroup:resourceGroup,AddressPrefix:addressSpace.addressPrefixes[0],SubnetName1:subnets[0].name,SubnetAddress1:subnets[0].addressPrefix,SubnetName2:subnets[1].name,SubnetAddress2:subnets[1].addressPrefix}",
 #   o: "table"
 # )
 
@@ -74,16 +75,28 @@ end
 #   o: "tsv"
 # )
 
-Azure.run("az network vnet subnet list",
-  ids:
-)
+# Azure.run("az network vnet list",
+#   o: "table"
+# )
 
-(1..3).each do |i|
-  Azure.run("az network nic create",
-    n: "nic#{i}",
-    g: "rg-vnet",
-    subnet: "subnet#{i}",
-    vnet_name: "vnet#{i}"
-  )
-end
+# (1..3).each do |i|
+#   Azure.run("az network nic create",
+#     n: "nic#{i}",
+#     g: "rg-vnet",
+#     subnet: "subnet#{i}",
+#     vnet_name: "vnet#{i}"
+#   )
+# end
 
+
+Azure.run("az vm create",
+  n: "vm3",
+  g:  "tamura",
+  image: "UbuntuLTS",
+  l: "japanwest",
+  size: "Basic_A0",
+  storage_sku: "Standard_LRS",
+  generate_ssh_keys: "",
+  subnet: "/subscriptions/xxxx/resourceGroups/Mail-WJP_H/providers/Microsoft.Network/virtualNetworks/Mail-WJP_Vnet_H/subnets/Mail-WJP_Vnet-SN_H",
+  debug: true
+)  
